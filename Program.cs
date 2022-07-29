b695109b4a01f1ba45b509706a10458a7c730a42
Sindre Andreassen <sindrema@gmail.com>
2022-07-30 01:36:34 +0200

    new file:   .vs/34Finder/DesignTimeBuild/.dtbcache.v2 	new file:   .vs/34Finder/FileContentIndex/9f4dd598-3a87-4353-99b3-236dcf8fbfa6.vsidx 	new file:   .vs/34Finder/FileContentIndex/f60f4f47-877d-4e7d-a0db-05bbee887ad3.vsidx 	new file:   .vs/34Finder/FileContentIndex/read.lock 	new file:   .vs/34Finder/v17/.futdcache.v1 	new file:   .vs/34Finder/v17/.suo 	new file:   .vs/ProjectEvaluation/34finder.metadata.v2 	new file:   .vs/ProjectEvaluation/34finder.projects.v2 	new file:   34Finder.csproj 	new file:   34Finder.sln 	new file:   Program.cs 	new file:   bin/Debug/net6.0/34Finder.deps.json 	new file:   bin/Debug/net6.0/34Finder.dll 	new file:   bin/Debug/net6.0/34Finder.exe 	new file:   bin/Debug/net6.0/34Finder.pdb 	new file:   bin/Debug/net6.0/34Finder.runtimeconfig.json 	new file:   bin/Debug/net6.0/GBX.NET.Imaging.dll 	new file:   bin/Debug/net6.0/GBX.NET.Json.dll 	new file:   bin/Debug/net6.0/GBX.NET.LZO.dll 	new file:   bin/Debug/net6.0/GBX.NET.PAK.dll 	new file:   bin/Debug/net6.0/GBX.NET.dll 	new file:   bin/Debug/net6.0/Microsoft.Extensions.Logging.Abstractions.dll 	new file:   bin/Debug/net6.0/Microsoft.Win32.SystemEvents.dll 	new file:   bin/Debug/net6.0/Newtonsoft.Json.dll 	new file:   bin/Debug/net6.0/ShellProgressBar.dll 	new file:   bin/Debug/net6.0/System.Drawing.Common.dll 	new file:   bin/Debug/net6.0/TmEssentials.dll 	new file:   bin/Debug/net6.0/map.Gbx 	new file:   bin/Debug/net6.0/runtimes/unix/lib/netcoreapp3.0/System.Drawing.Common.dll 	new file:   bin/Debug/net6.0/runtimes/win/lib/netcoreapp3.0/Microsoft.Win32.SystemEvents.dll 	new file:   bin/Debug/net6.0/runtimes/win/lib/netcoreapp3.0/System.Drawing.Common.dll 	new file:   obj/34Finder.csproj.nuget.dgspec.json 	new file:   obj/34Finder.csproj.nuget.g.props 	new file:   obj/34Finder.csproj.nuget.g.targets 	new file:   obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   obj/Debug/net6.0/34Finder.AssemblyInfo.cs 	new file:   obj/Debug/net6.0/34Finder.AssemblyInfoInputs.cache 	new file:   obj/Debug/net6.0/34Finder.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net6.0/34Finder.GlobalUsings.g.cs 	new file:   obj/Debug/net6.0/34Finder.assets.cache 	new file:   obj/Debug/net6.0/34Finder.csproj.AssemblyReference.cache 	new file:   obj/Debug/net6.0/34Finder.csproj.BuildWithSkipAnalyzers 	new file:   obj/Debug/net6.0/34Finder.csproj.CopyComplete 	new file:   obj/Debug/net6.0/34Finder.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net6.0/34Finder.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net6.0/34Finder.dll 	new file:   obj/Debug/net6.0/34Finder.genruntimeconfig.cache 	new file:   obj/Debug/net6.0/34Finder.pdb 	new file:   obj/Debug/net6.0/apphost.exe 	new file:   obj/Debug/net6.0/ref/34Finder.dll 	new file:   obj/Debug/net6.0/refint/34Finder.dll 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using GBX.NET;
+using GBX.NET.Engines.Game;
+using GBX.NET.LZO;
+
+
+public (float, int) GetAverage(string path, bool ignoreIngame = true)
+{
+    var replay = GameBox.ParseNode<CGameCtnReplayRecord>(path);
+    var ls = replay?.Ghosts?.FirstOrDefault()?.RecordData?.Samples;
+    if (ls == null) throw new Exception("No samples found");
+
+    var blocklist = new List<float>(){ -1, 0, 1};
+    var actionKeys = new List<float>(){ };
+
+    var matches = ls.GroupBy(x=> x.Steer).Where(x=> !blocklist.Any(c=> c == x.Key) );
+    
+
+}
+
+
+
+var wa = Stopwatch.StartNew();
+var ls = replay.Ghosts.FirstOrDefault().RecordData.Samples;
+
+    foreach (var replayRecord in ls)
+    {
+        while (wa.ElapsedMilliseconds < replayRecord.Timestamp.Value.TotalMilliseconds)
+        {
+        }
+
+        var index = ls.IndexOf(replayRecord);
+    if (index > 2)
+    {
+        var lastItem = ls[index - 1];
+        var lastLastItem = ls[index - 1];
+        if (lastItem != null && lastLastItem != null)
+        {
+            if (lastLastItem.Steer == replayRecord.Steer && lastItem.Steer == replayRecord.Steer)
+            {
+                if (!(replayRecord.Steer == 1 || replayRecord.Steer == -1 || replayRecord.Steer == 0))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+            }
+        }
+    }
+        
+        Console.WriteLine($"[{wa.Elapsed.Minutes}:{wa.Elapsed.Seconds}:{wa.Elapsed.Milliseconds}] - {replayRecord.Steer.Value}");
+        Console.ForegroundColor = ConsoleColor.White;
+
+}
+
+
+Console.ReadKey();
